610f7ce5171f6e4d9799e86b8d02fbd61687e617
Tomás Rojas <tomas.rojas.c@ug.uchile.cl>
2020-03-09 16:21:27 -0300

    Solved problem with dict_oif_utdf file (it was not created, now it offers you to make it)

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from modules.utils import UTDF_dict_from_df
+import pickle
+
+answer = input('do you have the file ./data/dicts_of_utdf.pickle? [y/n]: \n')
+if answer == 'n':
+
+    with open('data/UTdf.all_days', 'rb') as f:
+        dicts_of_utdf = UTDF_dict_from_df(pickle.load(f))
+    f.close()
+
+    with open('data/dicts_of_utdf.pickle', 'wb') as f:
+        pickle.dump(dicts_of_utdf, f)
+    f.close()
 import scripts.filtering
 import scripts.making_dicts
 import scripts.make_pdf
--- modules/utils.py
+++ modules/utils.py
@@ -36,13 +36,13 @@ def make_timeseries_plot(time_series_df, fig_size):
     """
 
     x = time_series_df.index
-    file1 = time_series_df.file1
-    file2 = time_series_df.file2
+    file1 = time_series_df.seeing_file1
+    file2 = time_series_df.seeing_file2
     date = time_series_df.index.date[0].strftime('%Y-%m-%d')
 
     df_interpolated = time_series_df.interpolate('slinear')
-    file1_inter = df_interpolated.file1
-    file2_inter = df_interpolated.file2
+    file1_inter = df_interpolated.seeing_file1
+    file2_inter = df_interpolated.seeing_file2
 
     fig = plt.figure(figsize=fig_size)
     plt.plot(x, file1_inter, color='blue')
@@ -56,3 +56,18 @@ def make_timeseries_plot(time_series_df, fig_size):
     plt.ylabel('seeing DIMM')
 
     return fig
+
+
+
+def UTDF_dict_from_df(df):
+    """
+    This funtion takes a df and returns a dict of utdf
+    :param df: dataframe
+    :return: dict
+    """
+    dict2return = {}
+    keys = [i for i in df['date_key'].unique()]
+    for key in keys:
+        print(key)
+        dict2return[key] = df[df['date_key']==key]
+    return dict2return
